00963b8b0fdf21ecb6eb2dbf1d032a3505531e7f
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2025-01-31 15:43:19 +0200

    Clean Code for Landing | Enhance reusable code | use High Quality Logo

--- scripts/slideHandler.js
+++ scripts/slideHandler.js
@@ -1,28 +1,22 @@
 // Handle Slide for Subscription Section
-
 document.addEventListener('DOMContentLoaded', function () {
     new Splide('#splide-subscription', {
         type: 'slide',
         perPage: 3,  // Number of visible slides
-        perMove: 1,
+        perMove: 1,  // Number of slides moved on a single slide move
+        rewind: true,
         gap: '2rem',
-        padding: {
-            left: '3rem',
-            right: '3rem',
-        },
         pagination: false,  // Hide pagination
         breakpoints: {
             1270: {
                 perPage: 2,
                 gap: '20px',
-                padding: {
-                    left: '3rem',
-                    right: '3rem',
-                },
+                focus: 'center',
             },
             825: {
                 perPage: 1,
                 gap: '10px',
+                focus: 'center',
                 padding: {
                     left: '20px',
                     right: '20px',
